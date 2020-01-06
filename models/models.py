b528fe26e7e07a69c6e473e043dc2831c8e95db7
zhimingd2 <shptxc@gmail.com>
2020-01-06 17:14:08 -0500

    add

--- models/models.py
+++ models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class zhiming_note(models.Model):
     _name = 'zhiming_note.zhiming_note'
 
-     name = fields.Char("MainName")
+    name = fields.Char("MainName")
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
