848561b8371ccaf6dd5c8490eda8a1056c1ad110
zhimingd2 <shptxc@gmail.com>
2020-01-06 19:56:13 -0500

    add

--- models/models.py
+++ models/models.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 
 class ZhimingNote(models.Model):
-    _name = "i.zhiming_note"
+    _name = "zhiming.note"
     _description = "Zhiming Note Test"
 
     name = fields.Char(string="NoteMain", request=True)
