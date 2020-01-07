0c7b23d50a0a3c66cea6fd0b4cd65ec780b4c690
zhimingd2 <shptxc@gmail.com>
2020-01-06 19:37:25 -0500

    add

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Human Resources',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
--- models/models.py
+++ models/models.py
@@ -2,14 +2,7 @@
 
 from odoo import models, fields, api
 
-class zhiming_note(models.Model):
-    _name = 'zhiming_note.zhiming_note'
+class ZhimingNote(models.Model):
+    _name = "zhiming_note.zhiming_note"
+    _description = "Zhiming Note Test"
 
-    name = fields.Char("MainName")
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
